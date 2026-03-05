f23f2e36bc82f6042b0420fd5eb3ec923c973b89
sukanto01899 <sukanto01899@gmail.com>
2026-03-05 21:16:25 +0600

    verification added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,6 +7,10 @@ import { WalletConnectQrModal } from "@/components/walletconnect-qr-modal";
 export const metadata: Metadata = {
   title: "Stacks DAO",
   description: "Token-governed treasury DAO on Stacks chain",
+  other: {
+    "talentapp:project_verification":
+      "c10afd5a36ab15a431c7083d2da03990702afc6f9d385f3e16ec0523d5424ba0923d35e3a517a035830830e722ed2ca9d2c43069de772deca273356d74d3cd2f",
+  },
 };
 
 export default function RootLayout({
